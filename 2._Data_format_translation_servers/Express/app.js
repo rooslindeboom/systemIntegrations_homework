8e041dcb8a0b1060ede01fee41b1eae01101afbb
rooslindeboom <rooslindeboom@outlook.com>
2022-09-17 15:54:56 +0200

    Set up express endpoints for CVS and Yaml

--- 2._Data_format_translation_servers/Express/app.js
+++ 2._Data_format_translation_servers/Express/app.js
@@ -1,19 +1,37 @@
-import express from 'express'
+const express = require('express')
+const fs = require('fs');
+const yaml = require('js-yaml');
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Express: data format translation servers')
+});
 
 app.get('/cvs', (req, res) => {
-    res.send('Hello cvs!')
-  })
+  try {
+    fs.readFile('/Users/rooslindeboom/Desktop/KEA - projects/System integrations/SI-homework/systemIntegrations_homework/1._File_formats_bonanza/node/Article_files/Article.cvs', 'utf8', function (err, data) {
+      console.log("-- cvs --");  
+      res.send(data)
+    });
+  } catch (e) {
+    console.log(e);
+  } 
+});
 
 app.get('/yaml', (req, res) => {
-    res.send('Hello yaml!')
-  })  
+  try {
+      let fileContents = fs.readFileSync('/Users/rooslindeboom/Desktop/KEA - projects/System integrations/SI-homework/systemIntegrations_homework/1._File_formats_bonanza/node/Article_files/Article.yaml', 'utf8');
+      let data = yaml.load(fileContents);
+  
+      console.log("-- yaml --");
+      res.send(data);
+  } catch (e) {
+      console.log(e);
+  }
+});  
   
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
