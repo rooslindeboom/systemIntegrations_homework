5184b21c15383c02336b404b33aa65a088197195
rooslindeboom <rooslindeboom@outlook.com>
2022-09-17 16:06:19 +0200

    Set up fastapi endpoints for txt, xml and json

--- 2._Data_format_translation_servers/Fast_API/main.py
+++ 2._Data_format_translation_servers/Fast_API/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import json
+from xml.dom import minidom
 
 app = FastAPI()
 
@@ -9,12 +11,24 @@ async def root():
 
 @app.get("/txt")
 async def root():
-    return {"message": "Hello txt"}   
+    with open('/Users/rooslindeboom/Desktop/KEA - projects/System integrations/SI-homework/systemIntegrations_homework/1._File_formats_bonanza/python/Article_files/Article.txt') as f:
+        contents = f.read()
+        print(contents)
+    return {"message": contents}   
 
 @app.get("/xml")
 async def root():
-    return {"message": "Hello xml"} 
+    file = minidom.parse('/Users/rooslindeboom/Desktop/KEA - projects/System integrations/SI-homework/systemIntegrations_homework/1._File_formats_bonanza/python/Article_files/Article.xml')
+    article = file.getElementsByTagName('article')
+
+    for elem in article:
+        print(elem.firstChild.data)
+    return {"message": elem.firstChild.data} 
 
 @app.get("/json")
 async def root():
-    return {"message": "Hello json"}          
+    f = open('/Users/rooslindeboom/Desktop/KEA - projects/System integrations/SI-homework/systemIntegrations_homework/1._File_formats_bonanza/python/Article_files/Article.json')
+    data = json.load(f)
+    print(data['article'])
+    f.close()
+    return {"message": data['article']}          
